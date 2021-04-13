7a43fd78f2080df0a1ee51ba964e373866dfec28
pedro santos <pedrosotnas@gmail.com>
2021-04-13 10:29:07 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    return this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length - 1) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return Math.min(...this.items);
 
-  avg() {}
+  }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      const sum = this.items.reduce((a, b) => {
+        return a + b;
+      });
+
+      return sum
+    }
+  }
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      const avgItems = this.sum() / this.items.length;
+
+      return avgItems;
+    }
+  }
 }
 
 module.exports = SortedList;
